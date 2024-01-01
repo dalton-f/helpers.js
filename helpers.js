949fbdb7f4c3c1c4851314bcf03974402abe3e79
Dalton <daltonfiggdavies@gmail.com>
2024-01-01 08:26:20 +0000

    Add functions, comments and minified file

--- helpers.js
+++ helpers.js
@@ -1,4 +1,6 @@
-// helpers.js v1.2.0 | https://github.com/dalton-f/helpers.js
+// helpers.js v1.3.0 | https://github.com/dalton-f/helpers.js
+
+// DOM Manipulation
 
 getById = (id) => document.getElementById(id);
 
@@ -12,16 +14,34 @@ removeClass = (element, classes) => element.classList.remove(classes);
 
 toggleClass = (element, classes) => element.classList.toggle(classes);
 
+hasClass = (element, classes) => element.classList.contains(classes);
+
 getInnerText = (element) => element.innerText;
 
 getInnerHTML = (element) => element.innerHTML;
 
+setInnerText = (element, text) => (element.innerText = text);
+
+setInnerHTML = (element, html) => (element.innerHTML = html);
+
+// Storage
+
 getLocalStorage = (key) => localStorage.getItem(key);
 
 setLocalStorage = (key, value) => localStorage.setItem(key, value);
+
+clearLocalStorage = () => localStorage.clear();
+
+// Array Manipulation
 
 getTotal = (array) =>
   (sum = array.reduce(
     (accumulator, currentValue) => accumulator + currentValue,
     0
   ));
+
+getMax = (array) => array.reduce((max, item) => Math.max(max, item), -Infinity);
+
+getMin = (array) => array.reduce((max, item) => Math.min(max, item), -Infinity);
+
+getUniqueValues = (array) => [...new Set(array)];
